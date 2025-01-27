09cd547ea4e5d5fbd7e7e0e3004344aad5e54ddf
shubham94119 <115475820+shubham94119@users.noreply.github.com>
2025-01-27 19:32:03 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   host: "0.0.0.0", // Bind to all interfaces
+    port: process.env.PORT || 3000,
 })
